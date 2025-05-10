dc0a3b0cb09cee6c04cac84259747efc147aad95
Mika'il Vallie <mikailvallie6@gmail.com>
2025-05-10 12:04:52 +0200

    BookUpdateBuilder

--- LMS/src/main/java/org/example/Domain/Book.java
+++ LMS/src/main/java/org/example/Domain/Book.java
@@ -1,28 +1,65 @@
 package org.example.Domain;
 
+//Mika'il Vallie 230259200
+// Log: BookUpdateBuilder
 
 public class Book {
-    private String title;
+    private String title, subject , author;
     private int bookId;
-    private String subject;
-    private String author;
 
-    public Book(String title, int bookId, String subject, String author) {
-        this.title = title;
-        this.bookId = bookId;
-        this.subject = subject;
-        this.author = author;
+    private Book(){
+    }
+
+    private Book(Builder builder) {
+        this.title = builder.title
+        this.bookId = builder.bookId;
+        this.subject = builder.subject;
+        this.author = builder.author;
+    }
+
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "title='" + title + '\'' +
+                ", subject='" + subject + '\'' +
+                ", author='" + author + '\'' +
+                ", bookId=" + bookId +
+                '}';
+    }
+
+    public static class Builder{
+        private String title, subject, author;
+        private int bookId;
+
+        public Builder title(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder subject(String subject){
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder author(String author){
+            this.author = author;
+            return this;
+        }
+
+        public Builder bookId(int bookId){
+            this.bookId = bookId;
+            return this;
+        }
+
+        public Book build(){
+            return new Book(this);
+        }
+
+
     }
 
 
 
-    public void displayBook() {
-        System.out.println("Book: " + title + " by " + author);
-    }
-
-    public void updateBookStatus() {
-        System.out.println("Updating book status...");
-    }
 
 }
 
