a673bf012dd0e625df28150b4c3e0bb0b4228e11
Ayrton Williams <129041355+220086168@users.noreply.github.com>
2025-05-11 21:08:56 +0200

    Update Notification.java

--- LMS/src/main/java/org/example/Domain/Notification.java
+++ LMS/src/main/java/org/example/Domain/Notification.java
@@ -1,5 +1,7 @@
 package org.example.Domain;
 
+//Ayrton Williams 220086168
+
 import java.util.Date;
 
 public class Notification {
@@ -7,10 +9,47 @@ public class Notification {
     private Date dateCreated;
     private String content;
 
-    public Notification(int notificationId, Date dateCreated, String content) {
-        this.notificationId = notificationId;
-        this.dateCreated = dateCreated;
-        this.content = content;
+    private Notification(){
+    }
+
+    public Notification(Builder builder) {
+        this.notificationId = builder.notificationId;
+        this.dateCreated = builder.dateCreated;
+        this.content = builder.content;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "notificationID=" + notificationID +
+                ", createOn=" + createOn +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int notificationId;
+        private Date dateCreated;
+        private String content;
+
+        public Builder notificationId(int notificationId) {
+            this.notificationId = notificationId;
+            return this;
+        }
+
+        public Builder dateCreated(Date dateCreated) {
+            this.dateCreated = dateCreated;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Notification build() {
+            return new Notification(this);
+        }
     }
 
     public void sendNotification() {
