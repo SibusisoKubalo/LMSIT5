9850494724b61b430e622cc3b67cdffd778725ab
Mika'il Vallie <mikailvallie6@gmail.com>
2025-05-18 13:00:38 +0200

    Book Factory and Customer Factory
    Signed-off-by: Mika'il Vallie <mikailvallie6@gmail.com>

--- LMS/src/main/java/org/example/Factory/BookFactory.java
+++ LMS/src/main/java/org/example/Factory/BookFactory.java
@@ -2,8 +2,28 @@ package org.example.Factory;
 
 import org.example.Domain.Book;
 
+//Mika'il Vallie 230259200
+// Log:BookFactory
+
 public class BookFactory {
-    public static Book createBook(String title, int bookId, String subject, String author) {
-        return new Book(title, bookId, subject, author);
+
+    public static Book createBook(String title, String subject, String author, int bookId) {
+        return new Book.Builder()
+                .title(title)
+                .subject(subject)
+                .author(author)
+                .bookId(bookId)
+                .build();
+    }
+
+    public static Book createBookWithTitleAndAuthor(String title, String author) {
+        return new Book.Builder()
+                .title(title)
+                .author(author)
+                .build();
+    }
+
+    public static Book createEmptyBook() {
+        return new Book.Builder().build();
     }
 }
