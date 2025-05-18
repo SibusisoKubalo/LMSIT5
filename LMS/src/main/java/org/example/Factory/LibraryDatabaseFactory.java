dd3b4ded9a2d89ae268a3d43cf481831dc2b7e34
Ayrton Williams <129041355+220086168@users.noreply.github.com>
2025-05-18 20:45:19 +0200

    Update LibraryDatabaseFactory.java

--- LMS/src/main/java/org/example/Factory/LibraryDatabaseFactory.java
+++ LMS/src/main/java/org/example/Factory/LibraryDatabaseFactory.java
@@ -2,10 +2,12 @@ package org.example.Factory;
 
 
 import org.example.Domain.LibraryDatabase;
+import org.example.Domain.Book;
 
 public class LibraryDatabaseFactory {
-    public static LibraryDatabase createLibraryDatabase() {
-        return new LibraryDatabase();
+    public static LibraryDatabase createLibraryDatabase(List<Book> books) {
+        return new LibraryDatabase().Builder()
+                .Book(books)
+                .build();
     }
 }
-
