35f1a7a5828f6dca490e74ef818049155a43e337
Ayrton Williams <129041355+220086168@users.noreply.github.com>
2025-05-18 20:48:42 +0200

    Update NotificationFactory.java

--- LMS/src/main/java/org/example/Factory/NotificationFactory.java
+++ LMS/src/main/java/org/example/Factory/NotificationFactory.java
@@ -1,12 +1,23 @@
 package org.example.Factory;
 
+//Ayrton Williams 220086168
+
 import org.example.Domain.Notification;
 
 import java.util.Date;
 
 public class NotificationFactory {
-    public static Notification createNotification(int notificationId, String content) {
-        return new Notification(notificationId, new Date(), content);
+
+    public static Notification ValidateAndCreateNotification(int notificationId, String content) {
+
+        if (notificationId <= 0 || notificationId > 1000) // this number can be changed if needed
+            return null;
+        if (content.isEmpty() || content.equals("") || content.equals("null"))
+            return null;
+
+        return new Notification.Builder()
+                .notificationId(notificationId)
+                .content(content)
+                .build();
     }
 }
-
