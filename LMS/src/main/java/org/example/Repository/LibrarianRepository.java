3b0b6d3807014a473ccfcc8a7e38fbe6ca6c7c35
Sibusiso Kubalo <sibusiso.kubalo@traderoot.com>
2025-05-20 16:47:51 +0200

    Librarian Update

--- LMS/src/main/java/org/example/Repository/LibrarianRepository.java
+++ LMS/src/main/java/org/example/Repository/LibrarianRepository.java
@@ -1,21 +1,45 @@
 package org.example.Repository;
+/**
+ * LibrarianRepository.java
+ *
+ * Author: Sibusiso Kubalo
+ * Student Num: 218316038
+ */
 
 import org.example.Domain.Librarian;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarianRepository {
-    private final Map<Integer, Librarian> librarianMap = new HashMap<>();
+    private Map<Integer, Librarian> librarianMap = new HashMap<>();
 
-    public void save(Librarian librarian) {
+    public Librarian save(Librarian librarian) {
         librarianMap.put(librarian.getId(), librarian);
+        return librarian;
     }
 
     public Librarian findById(int id) {
         return librarianMap.get(id);
     }
 
-    public void delete(int id) {
-        librarianMap.remove(id);
+    public Librarian update(Librarian librarian) {
+        if (librarianMap.containsKey(librarian.getId())) {
+            librarianMap.put(librarian.getId(), librarian);
+            return librarian;
+        }
+        return null;
+    }
+
+    public boolean delete(int id) {
+        if (librarianMap.containsKey(id)) {
+            librarianMap.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Map<Integer, Librarian> findAll() {
+        return librarianMap;
     }
 }
--- LMS/src/main/java/org/example/Service/LibrarianService.java
+++ LMS/src/main/java/org/example/Service/LibrarianService.java
@@ -1,21 +1,36 @@
 package org.example.Service;
+/**
+ * LibrarianRepository.java
+ *
+ * Author: Sibusiso Kubalo
+ * Student Num: 218316038
+ */
 
 import org.example.Domain.Librarian;
 import org.example.Repository.LibrarianRepository;
 
+import java.util.Map;
+
 public class LibrarianService {
-    private final LibrarianRepository librarianRepository = new LibrarianRepository();
+    private LibrarianRepository repository = new LibrarianRepository();
 
-    public void addLibrarian(Librarian librarian) {
-        librarianRepository.save(librarian);
+    public Librarian createLibrarian(Librarian librarian) {
+        return repository.save(librarian);
     }
 
-    public Librarian findLibrarian(int id) {
-        return librarianRepository.findById(id);
+    public Librarian getLibrarianById(int id) {
+        return repository.findById(id);
     }
 
-    public void deleteLibrarian(int id) {
-        librarianRepository.delete(id);
+    public Librarian updateLibrarian(Librarian librarian) {
+        return repository.update(librarian);
+    }
+
+    public boolean deleteLibrarian(int id) {
+        return repository.delete(id);
+    }
+
+    public Map<Integer, Librarian> getAllLibrarians() {
+        return repository.findAll();
     }
 }
-
